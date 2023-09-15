d2d8db9383d51a553bae610aa4e8c76e47af240c
Shydeev <hday5297@gmail.com>
2023-09-15 13:23:57 +0300

    End of  section 2.

---
+++ Persistence/Migrations/20230915094219_InitialCreate_updated.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class InitialCreate_updated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tilte",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Tilte");
+        }
+    }
+}
